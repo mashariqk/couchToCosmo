1d9f37bab3798a99d345d62b00475b748e7d6dd5
Mashariq Khan <mashariq@outlook.com>
2018-02-26 09:45:20 -0600

    Added loop to keep trying to push data in case of failure

--- src/com/footlocker/azure/InsertJson.java
+++ src/com/footlocker/azure/InsertJson.java
@@ -2,8 +2,10 @@ package com.footlocker.azure;
 
 import com.microsoft.azure.documentdb.DocumentClient;
 import com.microsoft.azure.documentdb.DocumentClientException;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InsertJson implements Runnable{
@@ -25,15 +27,31 @@ public class InsertJson implements Runnable{
 
     @Override
     public void run() {
-        try {
+        List<String> remainder = new ArrayList<>();
             for (String url:this.fileList){
-                GeneralUtils.createRowDocumentIfNotExists(this.client, this.databaseName, this.collectionName, (Row) GeneralUtils.parseJson(couch.getHttpResponse(url).getBytes(), Row.class));
+                try {
+                    GeneralUtils.createRowDocumentIfNotExists(this.client, this.databaseName, this.collectionName, (Row) GeneralUtils.parseJson(couch.getHttpResponse(url).getBytes(), Row.class));
+                } catch (Exception e) {
+                    remainder.add(url);
+                    e.printStackTrace();
+                }
+            }
+            while (true){
+                if (CollectionUtils.isEmpty(remainder)){
+                    break;
+                }
+                ArrayList<String> listForOperation = new ArrayList<>();
+                listForOperation.addAll(remainder);
+                remainder  = new ArrayList<>();
+                for (String url:listForOperation){
+                    try {
+                        GeneralUtils.createRowDocumentIfNotExists(this.client, this.databaseName, this.collectionName, (Row) GeneralUtils.parseJson(couch.getHttpResponse(url).getBytes(), Row.class));
+                    } catch (Exception e) {
+                        remainder.add(url);
+                        e.printStackTrace();
+                    }
+                }
             }
             System.out.println(" Finished Thread "+ ++counter);
-        } catch (DocumentClientException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
